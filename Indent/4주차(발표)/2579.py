eb7476864c7d9ee5347fdab896ef41393b316f42
조영진(Joyoungjin) <slugger613@naver.com>
2024-05-10 14:54:00 +0900

    24.05.10(금)

---
+++ Indent/4주차(발표)/2579.py
@@ -0,0 +1,23 @@
+import sys
+sinput = sys.stdin.readline
+
+n = int(sinput())
+lst = [0]
+lower, upper = [0], [0] # lower는 전의 것을 밟지 않은 경우 upper는 전의 것을 밟은 경우
+
+for _ in range(n):
+    lst.append(int(sinput()))
+
+tmplst = [0 for _ in range(len(lst))]
+tmplst[1]= lst[1]
+lower.append(lst[1])
+upper.append(lst[1])
+
+for i in range(2, n+1):
+    low = lst[i] + tmplst[i-2]
+    up = lst[i] + lower[i-1]
+    lower.append(low)
+    upper.append(up)
+    tmplst[i] = max(lower[i], upper[i])
+
+print(tmplst[-1])
--- Indent/공통문제3/1654.py
+++ Indent/공통문제3/1654.py
@@ -7,15 +7,16 @@ lst = []
 for _ in range(n):
     lst.append(int(sinput()))
 
-start, end = 1, max(lst)//(k//n)
+start, end = 1, max(lst)//(k//n) 
+# 가장 큰 랜선은 적어도 k//n개로는 나뉘어야 함
 mid = 0
 while start <= end: # 이분탐색 시작
     total = 0
-    mid = (start + end) // 2
+    mid = (start + end) // 2 # 탐색 범위의 중앙값
 
     for i in lst:
-        total += i // mid
-
+        total += i // mid # 중앙값으로 나눈 몫
+        
     if total >= k:
         start = mid + 1 # 이미 start는 mid에서 고려되었기 때문에 +1
     else:
