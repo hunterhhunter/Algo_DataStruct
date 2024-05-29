d2cd9b5209cb282ac27ec2fa0ec5d86f8900d8dd
조영진(Joyoungjin) <slugger613@naver.com>
2024-05-29 10:41:44 +0900

    24.05.29

---
+++ 5.동적계획법1/2565.py
@@ -0,0 +1,17 @@
+import sys
+sinput = sys.stdin.readline
+
+n = int(sinput())
+lst = []
+table = [1 for _ in range(n)]
+for _ in range(n):
+    lst.append(list(map(int, sinput().split())))
+
+lst.sort()
+
+for i in range(1, n):
+    for j in range(i):
+        if lst[j][1] < lst[i][1]:
+            table[i] = max(table[i], table[j]+1)
+
+print(n-max(table))
